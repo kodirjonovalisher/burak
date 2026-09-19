7939c79bf3e910882ad328a0942a19270849e9f0
Alisher <kodirjonovalisher@gmail.com>
2026-09-19 20:42:51 +0900

    BRR: Intial Setup

---
+++ dist/server.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("EXUCUATED");
+console.log("EXUCUATED");
+const moment_1 = __importDefault(require("moment"));
+const currentTime = (0, moment_1.default)().format("YY DD MM");
+console.log(currentTime);
+const person = "ALEX";
+const count = 100;
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+// console.log("EXUCUATED");
+// console.log("EXUCUATED");
+
+// import moment from "moment";
+
+// const currentTime = moment().format("YY DD MM");
+// console.log(currentTime);
+
+// const person: string = "ALEX";
+// const count: number = 100;
