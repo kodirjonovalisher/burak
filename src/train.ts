47ea272f6cab7b6c8bc967227fbac88bb73ac237
Alisher <kodirjonovalisher@gmail.com>
2026-09-22 22:10:46 +0900

    feat: MIT M-TASK bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,16 @@
-console.log("HEllo world")
+// MIT M-TASK
+
+function getSquareNumbers(arr: number[]) {
+    let result = [];
+
+    for (let x of arr) {
+        result.push({
+            Raqam: x,
+            Kvadrati: x * x
+        });
+    }
+    return result;
+}
+
+console.log(getSquareNumbers([4, 7, 8, 15, 20, 60,]));
+//console.log("HEllo world")
